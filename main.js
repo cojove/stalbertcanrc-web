08cb92b050b5a3c2cb577fc32814787067fb0fd1
cojove <48028762+cojove@users.noreply.github.com>
2019-03-03 20:52:49 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ function scrollToId(id) {
     let el = $("#" + id);
     $("html, body").animate({scrollTop: el.offset().top}, "slow");
 }
+
+// Rest API
+fetch("https://app.churchsocial.com/api/sermons?limit=5", {
+  headers: {
+    Authorization: "0e3caa43db49cc8729a8bfa91f177bf6",
+    mode: "no-cors"
+  }
+}).then((rawData) => rawData.json())
+.then((jsonData) => console.log(jsonData));
