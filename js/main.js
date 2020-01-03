2371d649b6331f628ab0744008df40d7cafd7227
cojove <48028762+cojove@users.noreply.github.com>
2020-01-02 22:07:39 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Get the current year for the copyright
+$('#year').text(new Date().getFullYear());
+
+// Hide/Show Scroll Back to Top Arrow
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('#back-to-top').fadeIn();
+  } else {
+    $('#back-to-top').fadeOut();
+  }
+});
+
+// scroll body to 0px on click of Back to Top Arrow
+$('#back-to-top').click(function () {
+  $('body,html').animate({
+    scrollTop: 0
+  }, 800);
+  return false;
+});
+
+// Stop Sermon Audio and Video on Modal Close
+$(".modal").on("hide.bs.modal", function (e) {
+  e.target.querySelector("audio").pause();
+  $("iframe").attr("src", "");
+});
